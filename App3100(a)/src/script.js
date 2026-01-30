2011b8c21bbabe9fbd1ca595b36d1f7bfb9e4e58
SSH-Excelencia <sshexcelenciaensalud@gmail.com>
2026-01-30 15:27:23 -0500

    Update script.js

--- App3100(a)/src/script.js
+++ App3100(a)/src/script.js
@@ -140,8 +140,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // Cargar Excel solo una vez
     if (!bloque2.dataset.loaded) {
-      cargarExcel("lista-chequeo.xlsx");  //local
-      //cargarExcel("public/lista-chequeo.xlsx");  //web
+      //cargarExcel("lista-chequeo.xlsx");  //local
+      cargarExcel("public/lista-chequeo.xlsx");  //web
       bloque2.dataset.loaded = "true";
     }
 
